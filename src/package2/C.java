35f5b2c1a7800eb5b885a6a7066e023527c6d3cb
eunjaelim <imeunjae361@gmail.com>
2024-05-10 12:15:43 +0900

    Initial commit

---
+++ src/package2/C.java
@@ -0,0 +1,13 @@
+package package2;
+import package1.*;
+
+public class C {
+
+    public String PublicMessage = "This is public";
+    protected String protectedMessage = "This is protected";
+    private String privateMessage = "This is private";
+
+    String defaultMessage = "This is the default";
+
+
+}
