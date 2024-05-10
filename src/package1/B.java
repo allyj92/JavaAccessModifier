f64e14e17c6be18ae3ef48f138979e8d13cf8503
eunjaelim <imeunjae361@gmail.com>
2024-05-10 12:31:01 +0900

    수정

--- src/package1/B.java
+++ src/package1/B.java
@@ -1,4 +1,9 @@
 package package1;
+import package2.*;
 
-public class B {
+public class B extends C {
+    public static void main(String[] args){
+        C c = new C();
+//        System.out.println(c.protectedMessage);//error
+    }
 }
